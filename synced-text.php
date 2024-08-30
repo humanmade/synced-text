8e00547b8f5c02b0a84e0dc8d740bc0962f18566
Robert O'Rourke <23417+roborourke@users.noreply.github.com>
2024-08-30 18:15:10 +0100

    Fix textdomain in plugin header

--- synced-text.php
+++ synced-text.php
@@ -7,7 +7,7 @@
  * Author URI: https://humanmade.com
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: seon
+ * Text Domain: synced-text
  * Domain Path: /languages
  */
 
